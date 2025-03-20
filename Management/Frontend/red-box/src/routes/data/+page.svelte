a326b871021a7f04906dade394ab0874303f21c6
Izabela Marczewska <izamarczewska0@gmail.com>
2025-03-20 22:39:56 +0100

    Frontend for data module

--- Management/Frontend/red-box/src/routes/data/+page.svelte
+++ Management/Frontend/red-box/src/routes/data/+page.svelte
@@ -1,2 +1,12 @@
-<h2>Data</h2>
-<p>This page is under development.</p>
+<script>
+    import { env } from "$env/dynamic/public";
+  
+    const kibana_ip = env.PUBLIC_KIBANA_IP === "" ? "localhost" : env.PUBLIC_KIBANA_IP;
+    const kibana_port = env.PUBLIC_KIBANA_PORT === "" ? "8888" : env.PUBLIC_KIBANA_PORT;
+    const kibana_url = `http://${kibana_ip}:${kibana_port}`;
+  </script>
+  
+  <h2>Kibana Dashboard</h2>
+  <br>
+  <a href={kibana_url} target="_blank" class="action-button">Open Kibana</a>
+  
