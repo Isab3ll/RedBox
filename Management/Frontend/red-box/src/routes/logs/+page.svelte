9eb4f2cdf03be635c9f91d8da2560d4f4d5e1d23
Izabela Marczewska <izamarczewska0@gmail.com>
2025-03-29 00:11:56 +0100

    Prometheus metrics in Grafana

--- Management/Frontend/red-box/src/routes/logs/+page.svelte
+++ Management/Frontend/red-box/src/routes/logs/+page.svelte
@@ -3,9 +3,9 @@
   
   const grafana_ip = env.PUBLIC_GRAFANA_IP === "" ? "localhost" : env.PUBLIC_GRAFANA_IP;
   const grafana_port = env.PUBLIC_GRAFANA_PORT === "" ? "3000" : env.PUBLIC_GRAFANA_PORT;
-  const logs_dashboard_url = `http://${grafana_ip}:${grafana_port}/public-dashboards/541eb4e64f5c4ed1aca1c2cd84b4a65e`;
+  const logs_dashboard_url = `http://${grafana_ip}:${grafana_port}/d/feh4mpln2k7b4f/logs?orgId=1&from=now-1h&to=now&timezone=browser`;
 </script>
-  
+
 <h2>Grafana Logs Dashboard</h2>
 <div class="iframe-container">
   <iframe src={logs_dashboard_url} title="Grafana Logs Dashboard"></iframe>
--- Management/Frontend/red-box/src/routes/metrics/+page.svelte
+++ Management/Frontend/red-box/src/routes/metrics/+page.svelte
@@ -3,9 +3,9 @@
   
   const grafana_ip = env.PUBLIC_GRAFANA_IP === "" ? "localhost" : env.PUBLIC_GRAFANA_IP;
   const grafana_port = env.PUBLIC_GRAFANA_PORT === "" ? "3000" : env.PUBLIC_GRAFANA_PORT;
-  const metrics_dashboard_url = `http://${grafana_ip}:${grafana_port}/public-dashboards/541eb4e64f5c4ed1aca1c2cd84b4a65e`;
+  const metrics_dashboard_url = `http://${grafana_ip}:${grafana_port}/d/pMEd7m0Mz/metrics?orgId=1&from=now-1h&to=now&timezone=browser&var-host=$__all&var-container=$__all`;
 </script>
-  
+
 <h2>Grafana Metrics Dashboard</h2>
 <div class="iframe-container">
   <iframe src={metrics_dashboard_url} title="Grafana Metrics Dashboard"></iframe>
